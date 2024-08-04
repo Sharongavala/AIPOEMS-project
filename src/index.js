d3c80e7603960f3b0e2ce02797fe2d6ce907948d
Sharongavala <143581815+Sharongavala@users.noreply.github.com>
2024-08-04 17:03:14 +0300

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "La poésie est ici..",
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
